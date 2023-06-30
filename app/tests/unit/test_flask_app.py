ab0e8a2dcadd8b72bd16c7177015c6f6a0fac5ee
João Marco <j.marcogomes18@gmail.com>
2023-06-30 17:29:42 -0300

    feat: add tests for database connection

--- app/tests/unit/test_flask_app.py
+++ app/tests/unit/test_flask_app.py
@@ -1,4 +1,5 @@
 from src import create_app
+from src.database.database import db
 
 def test_home_page(setup):
     """
@@ -15,3 +16,14 @@ def test_home_page(setup):
     with flask_app.test_client() as test_client:
         response = test_client.get('/')
         assert response.status_code == 200
+
+def test_database_connection(setup):
+    flask_app = create_app()
+    flask_app.config.from_object('src.config.TestingConfig')
+
+    with flask_app.test_client():
+        db.create_all()
+        connection = db.engine.connect()
+        assert connection
+        connection.close()
+        db.drop_all()
